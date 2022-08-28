cf016760167ff99436339f6fc747d8b33a5f2004
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2022-08-28 14:07:25 +0300

    Вывод данных ACF в шаблонах. Разработка шаблона для тренеров

--- wp-content/themes/sportisland/archive-trainers.php
+++ wp-content/themes/sportisland/archive-trainers.php
@@ -1,56 +1,53 @@
-<?php 
+<?php
 get_header();
 ?>
 
-    <main class="main-content">
-      <div class="wrapper">
-        <ul class="breadcrumbs">
-          <li class="breadcrumbs__item breadcrumbs__item_home">
-            <a href="index.html" class="breadcrumbs__link">Главная</a>
-          </li>
-          <li class="breadcrumbs__item">
-            <a href="trainers.html" class="breadcrumbs__link">Тренеры</a>
-          </li>
+<main class="main-content">
+  <div class="wrapper">
+    <ul class="breadcrumbs">
+      <li class="breadcrumbs__item breadcrumbs__item_home">
+        <a href="index.html" class="breadcrumbs__link">Главная</a>
+      </li>
+      <li class="breadcrumbs__item">
+        <a href="trainers.html" class="breadcrumbs__link">Тренеры</a>
+      </li>
+    </ul>
+  </div>
+  <section class="trainers">
+    <div class="wrapper">
+      <h1 class="main-heading trainers__h">Тренеры</h1>
+      <?php
+      if (have_posts()) :
+      ?>
+        <ul class="trainers-list">
+          <?php
+          while (have_posts()) :
+            the_post();
+          ?>
+            <li class="trainers-list__item">
+              <article class="trainer">
+                <img src="<?php echo get_field('trainer_pic')['url']; ?>" alt="<?php echo get_field('trainer_pic')['alt']; ?>" class="trainer__thumb">
+                <div class="trainer__wrap">
+                  <h2 class="trainer__name">
+                    <?php the_title(); ?>
+                  </h2>
+                  <p class="trainer__text">
+                    <?php the_field('trainer_decription'); ?>
+                  </p>
+                </div>
+                <a href="#" class="trainer__subscribe btn">записаться</a>
+              </article>
+            </li>
+          <?php endwhile; ?>
         </ul>
-      </div>
-      <section class="trainers">
-        <div class="wrapper">
-          <h1 class="main-heading trainers__h">Тренеры</h1>
-          <ul class="trainers-list">
-            <li class="trainers-list__item">
-              <article class="trainer">
-                <img src="img/trainers__trainer_pic1.png" alt="" class="trainer__thumb">
-                <div class="trainer__wrap">
-                  <h2 class="trainer__name"> Комзова Ольга </h2>
-                  <p class="trainer__text"> Сертифицированный специалист по Пилатесу (Matwork, Ring, Ball, miniball) и ZUMBA, ZUMBA by strong. </p>
-                </div>
-                <a href="#" class="trainer__subscribe btn">записаться</a>
-              </article>
-            </li>
-            <li class="trainers-list__item">
-              <article class="trainer">
-                <img src="img/trainers__trainer_pic2.png" alt="" class="trainer__thumb">
-                <div class="trainer__wrap">
-                  <h2 class="trainer__name"> Юзвак Дмитрий </h2>
-                  <p class="trainer__text"> Хореограф. Преподаватель Dance Mix, Twerk, Strip dance и др направлений. Официальный представитель UBPF. Чемпионка Европы по фитнес Бикини. Тренер по дефиле. Персональный тренер тренажерного зала. </p>
-                </div>
-                <a href="#" class="trainer__subscribe btn">записаться</a>
-              </article>
-            </li>
-            <li class="trainers-list__item">
-              <article class="trainer">
-                <img src="img/trainers__trainer_pic3.png" alt="" class="trainer__thumb">
-                <div class="trainer__wrap">
-                  <h2 class="trainer__name"> Рудь Валерий </h2>
-                  <p class="trainer__text"> Персональный тренер. МСМК по пауерлифтингу, неоднократный победитель соревнований. </p>
-                </div>
-                <a href="#" class="trainer__subscribe btn">записаться</a>
-              </article>
-            </li>
-          </ul>
-        </div>
-      </section>
-    </main>
+      <?php
+      else :
+        get_template_part('tmp/no-post');
+      endif;
+      ?>
+    </div>
+  </section>
+</main>
 
 <?php
 get_footer();
